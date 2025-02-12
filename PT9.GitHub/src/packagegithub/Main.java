c56b59795849b1e42b0fb0660dd1c42a61b17c3a
Annn-03 <aesplugas@insgabrielamistral.cat>
2025-02-12 11:27:10 +0100

    Update Main.java

--- PT9.GitHub/src/packagegithub/Main.java
+++ PT9.GitHub/src/packagegithub/Main.java
@@ -2,7 +2,7 @@ package packagegithub;
 
 public class Main {
 	public static void main (String[] args) {
-		System.out.println("La modificació Git");
+		System.out.println("2a modificació GitHub");
 	}
 
 }
